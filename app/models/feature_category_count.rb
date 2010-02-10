af323fef2ca8600626437acb3f8c4ed02927b93d
Andres Montano <amontano@virginia.edu>
2010-02-10 12:02:34 -0600

    Using custom headers.

--- app/models/feature_category_count.rb
+++ app/models/feature_category_count.rb
@@ -1,4 +1,5 @@
 class FeatureCategoryCount < PlacesResource
+  headers['Host'] = PlacesResource.headers['Host']
   self.site = "#{self.site.to_s}categories/:category_id/"
   self.element_name = 'count'
 end
--- app/models/places_resource.rb
+++ app/models/places_resource.rb
@@ -1,11 +1,18 @@
 class PlacesResource < ActiveResource::Base
   hostname = Socket.gethostname.downcase
   if hostname == 'sds6.itc.virginia.edu'
-    self.site = 'http://staging.places.thdl.org/'
+    self.site = 'http://127.0.0.1/'
+    headers['Host'] = 'staging.places.thlib.org'
   elsif hostname == 'dev.thlib.org'
-    self.site = 'http://dev.places.thlib.org/'
+    self.site = 'http://127.0.0.1/'
+    headers['Host'] = 'dev.places.thlib.org'
+  elsif hostname.ends_with? 'local'
+    self.site = 'http://localhost/master/places/'
+  elsif hostname =~ /sds[3-8].itc.virginia.edu/
+    self.site = 'http://127.0.0.1/'
+    headers['Host'] = 'places.thlib.org'
   else
-    self.site = 'http://places.thlib.org/' # 'http://localhost/trunk/places/'
-  end
+    self.site = 'http://places.thlib.org/'
+  end  
   self.timeout = 100
 end
