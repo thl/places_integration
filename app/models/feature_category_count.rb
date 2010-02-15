915163f43aefdd1b751ae1f9de7a198587184f4e
Andres Montano <amontano@virginia.edu>
2010-02-15 10:20:29 -0600

    Header can't be blank. Better to not save at all.

--- app/models/feature_category_count.rb
+++ app/models/feature_category_count.rb
@@ -1,5 +1,5 @@
 class FeatureCategoryCount < PlacesResource
-  headers['Host'] = PlacesResource.headers['Host']
+  headers['Host'] = PlacesResource.headers['Host'] if !PlacesResource.headers['Host'].blank?
   self.site = "#{self.site.to_s}categories/:category_id/"
   self.element_name = 'count'
 end
