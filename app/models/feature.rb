004184774a58e132de33fc2da01e222c33149cb1
Andres Montano <amontano@virginia.edu>
2010-08-10 13:04:03 -0600

    adding find by geo code to api

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,3 +1,7 @@
 class Feature < PlacesResource
   headers['Host'] = PlacesResource.headers['Host'] if !PlacesResource.headers['Host'].blank?
+  
+  def self.find_by_geo_code(geo_code_type, geo_code)
+    Feature.get("by_geo_code/#{geo_code}", :geo_code_type => geo_code_type)
+  end
 end
