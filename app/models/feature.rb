11e02f0916131abfcdf091bef7c843464aed891b
Andres Montano <amontano@virginia.edu>
2010-08-10 16:32:44 -0600

    adding feature_type_ids to api

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -4,4 +4,12 @@ class Feature < PlacesResource
   def self.find_by_geo_code(geo_code_type, geo_code)
     Feature.get("by_geo_code/#{geo_code}", :geo_code_type => geo_code_type)
   end
+  
+  def feature_type_ids
+    if self.feature_type.instance_of? Array
+      return self.feature_type.collect{|type| type.id.to_i }
+    else
+      return [self.feature_type.id.to_i]
+    end
+  end
 end
