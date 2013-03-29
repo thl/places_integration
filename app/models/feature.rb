1217a26ea7ed84ee1c25b583b0df28d9c3005db9
Andres Montano <amontano@virginia.edu>
2013-03-29 17:14:06 -0600

    handling oddities on active resource behavior due to caching

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -5,12 +5,13 @@ class Feature < PlacesResource
     Feature.get("by_geo_code/#{geo_code}", :geo_code_type => geo_code_type)
   end
   
+  # Using nested active resource. This is problematic when caching
   def feature_type_ids
     begin
       if self.feature_type.instance_of? Array
-        return self.feature_type.collect{|type| type.id.to_i }
+        return self.feature_type.collect{|type| (type.instance_of?(Hash) ? type['id'] : type.id).to_i }
       else
-        return [self.feature_type.id.to_i]
+        return [(self.feature_type.instance_of?(Hash) ? self.feature_type['id'] : self.feature_type.id).to_i]
       end
     rescue
       return []
