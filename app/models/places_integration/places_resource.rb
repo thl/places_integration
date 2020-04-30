9d800e21c6096e6bb54eff976db2f6f9f84436e1
Andres Montano <amontano@virginia.edu>
2020-04-30 01:17:10 -0600

    updating urls to not use sds exceptions

--- app/models/places_integration/places_resource.rb
+++ app/models/places_integration/places_resource.rb
@@ -2,18 +2,15 @@ module PlacesIntegration
   class PlacesResource < ActiveResource::Base
     case InterfaceUtils::Server.environment
     when InterfaceUtils::Server::DEVELOPMENT
-      self.site = 'http://127.0.0.1/'
-      headers['Host'] = "dev-places.#{InterfaceUtils::Server.get_domain}"
+      self.site = "http://dev-places.#{InterfaceUtils::Server.get_domain}"
     when InterfaceUtils::Server::STAGING
-      self.site = 'http://127.0.0.1/'
-      headers['Host'] = "staging-places.#{InterfaceUtils::Server.get_domain}"
+      self.site = "http://staging-places.#{InterfaceUtils::Server.get_domain}"
     when InterfaceUtils::Server::PRODUCTION
-      self.site = 'http://127.0.0.1/'
-      headers['Host'] = "places.#{InterfaceUtils::Server.get_domain}"
+      self.site = "http://places.#{InterfaceUtils::Server.get_domain}"
     when InterfaceUtils::Server::LOCAL
       self.site = 'http://localhost/shanti/places/'
     else
-      self.site = "http://places.#{InterfaceUtils::Server.get_domain}/"
+      self.site = "http://places.#{InterfaceUtils::Server.get_domain}"
     end
 
     self.timeout = 100
