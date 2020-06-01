f31fda1064b8926709d56a3c7f6217f75963a865
Andres Montano <amontano@virginia.edu>
2020-06-01 10:28:39 -0600

    getting rid of sds specific code

--- app/models/places_integration/feature_category_count.rb
+++ app/models/places_integration/feature_category_count.rb
@@ -1,6 +1,5 @@
 module PlacesIntegration
   class FeatureCategoryCount < ActiveResource::Base
-    headers['Host'] = PlacesResource.headers['Host'] if !PlacesResource.headers['Host'].blank?
     self.site = "#{PlacesResource.site.to_s}categories/:category_id/"
     self.element_name = 'count'
     self.format = :xml
