ce5f1bf66521c27f629a1c0a91e66411a1ecedd2
Andres Montano <amontano@virginia.edu>
2023-11-09 23:41:21 -0600

    fixing keyword arguments to be more compatible with ruby3

--- app/models/places_integration/topic.rb
+++ app/models/places_integration/topic.rb
@@ -1,6 +1,6 @@
 module PlacesIntegration
   class Topic < PlacesResource
-    def self.paginated_features(id, options = {})
+    def self.paginated_features(id, **options)
       hash = self.get("#{id}", options.reject{|k,v| v.nil?})
       WillPaginate::Collection.create(hash['current_page'], hash['per_page'], hash['total_entries']) do |pager|
         pager.replace(hash['features'])
